876b6fb60518b6cffa8310820b75691257089804
Luiz Doretto <luiz.n.doretto@gmail.com>
2022-09-01 15:41:19 -0300

    create button styled component

---
+++ src/components/Button/button.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Holder } from "./button.style";
+
+const Button = ({ model, onClick, text, theme, type }) => {
+  return (
+    <Holder
+      type={type}
+      model={model}
+      onClick={onClick}
+      theme={theme}
+    >
+      {text}
+    </Holder>
+  );
+};
+
+Button.propTypes = {
+  model: PropTypes.oneOf(["filled", "outline"]),
+  onClick: PropTypes.func,
+  text: PropTypes.string,
+  theme: PropTypes.oneOf(["primary", "secondary", "tertiary", "black"]),
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
+};
+
+Button.defaultProps = {
+  model: "filled",
+  onClick: () => {},
+  text: "Button",
+  theme: "primary",
+  type: "button",
+};
+
+export default Button;
---
+++ src/components/Button/button.style.js
@@ -0,0 +1,41 @@
+import styled from "@emotion/styled";
+import { rem, colors } from "../../styles/variables";
+import fonts from "../../styles/typography";
+
+function setColor(theme) {
+  switch (theme) {
+    case "black":
+      return colors.black;
+    case "primary":
+      return colors.primary;
+    case "secondary":
+      return colors.secondary;
+    case "tertiary":
+      return colors.tertiary;
+    default:
+      return colors.primary;
+  }
+}
+
+export const Holder = styled.button`
+  align-items: center;
+  background-color: ${({ model, theme }) =>
+    model === "filled" ? setColor(theme) : "transparent"};
+  border-color: ${({ theme }) => setColor(theme)};
+  border-style: ${({ model }) => (model === "outline" ? "solid" : "none")};
+  border-width: ${rem(1)};
+  color: ${({ model, theme }) =>
+    theme === "basic"
+      ? colors.black
+      : model === "filled"
+      ? colors.white
+      : setColor(theme)};
+  cursor: pointer;
+  display: flex;
+  gap: ${rem(16)};
+  justify-content: center;
+  padding: ${rem(7)} 0;
+  width: ${rem(111)};
+  ${fonts.s1Subtitle()}
+  font-weight: 700;
+`;
