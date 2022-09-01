cc5aff78d714c3e5e215683d712c8b1372997627
Luiz Doretto <luiz.n.doretto@gmail.com>
2022-09-01 15:44:59 -0300

    add modal login to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+
+import ModalLogin from "../components/ModalLogin";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    navigate('home')
+  }
+
   return (
-    <div>Hello World</div>
+    <ModalLogin
+      open={true}
+      title="Welcome to CodeLeap network!"
+      subtitle="Please enter your username"
+      onEnter={handleLogin}
+    />
   );
 }
 
