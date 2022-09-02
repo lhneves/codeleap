70b1e8b0980ed7f974211ecf3e7908d82a12f57c
Luiz Doretto <luiz.n.doretto@gmail.com>
2022-09-02 16:40:40 -0300

    refactor modify post component (add formik validation, axios and toasts)

--- src/components/ModifyPost/modifyPost.jsx
+++ src/components/ModifyPost/modifyPost.jsx
@@ -1,31 +1,140 @@
 import React from "react";
-import { rem } from "../../styles/variables";
+import { useSelector } from "react-redux";
 
 import Button from "../Button";
 import Input from "../Input";
 
-import { Container, Title, Subtitle } from "./modifyPost.style";
+import { toast } from 'react-toastify';
 
-const ModifyPost = ({ onSubmit, edit = false }) => {
+import {
+  Container,
+  Title,
+  Subtitle,
+  ErrorHolder,
+  Error,
+  ModifyPostForm,
+} from "./modifyPost.style";
+import { rem } from "../../styles/variables";
+
+import * as Yup from "yup";
+import { Formik, Field } from "formik";
+
+import axios from '../../services/axios';
+
+import { useSWRConfig } from 'swr';
+
+const ModifyPost = ({ edit = false, id = null, hasEdited = false }) => {
+  const { mutate } = useSWRConfig();
+
+  const username = useSelector((state) => state.auth.user);
+
+  const ModifyPostSchema = Yup.object().shape({
+    title: Yup.string().required(),
+    content: Yup.string().required(),
+  });
+
   return (
     <Container edit={edit}>
       <Title>{edit ? "Edit item" : "What’s on your mind?"}</Title>
-      <div style={{ marginBottom: `${rem(19)}` }}>
-        <Subtitle>Title</Subtitle>
-        <Input id="title" label="" placeholder="Hello world" />
-      </div>
-      <div style={{ marginBottom: `${rem(35)}` }}>
-        <Subtitle>Content</Subtitle>
-        <Input id="content" label="" placeholder="Content here" type="textarea" />
-      </div>
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+      <Formik
+        initialValues={{
+          title: "",
+          content: "",
+        }}
+        validationSchema={ModifyPostSchema}
+        onSubmit={async (data, {resetForm}) => {
+          const { title, content } = data;
+
+          if(edit){
+            const postInfo = {
+              title: title,
+              content: content,
+            }
+
+            const response = await axios.patch(`/careers/${id}/`, postInfo);
+            
+            if(response){
+              toast.success('Post has been edited succesfully.')
+              mutate('/careers/');
+              hasEdited({edited: true})
+            }
+          } else {
+            const postInfo = {
+              username: username,
+              title: title,
+              content: content,
+            }
+
+            const response = await axios.post(`/careers/`, postInfo);
+            
+            if(response){
+              mutate('/careers/');
+              resetForm({values: ''});
+            }
+          }
+        }}
+      >
+        {({ values }) => (
+          <ModifyPostForm style={{ width: "100%" }}>
+            <div style={{ marginBottom: `${rem(19)}` }}>
+              <Subtitle>Title</Subtitle>
+              <Field name="title">
+                {({ field, meta }) => (
+                  <div>
+                    <Input
+                      id="title"
+                      label=""
+                      name="input"
+                      type="text"
+                      placeholder="Hello world"
+                      {...field}
+                    />
+                    {meta.touched && meta.error && (
+                      <ErrorHolder>
+                        <Error>{meta.error}</Error>
+                      </ErrorHolder>
+                    )}
+                  </div>
+                )}
+              </Field>
+            </div>
+            <div style={{ marginBottom: `${rem(35)}` }}>
+              <Subtitle>Content</Subtitle>
+              <Field name="content">
+                {({ field, meta }) => (
+                  <div>
+                    <Input
+                      id="content"
+                      label=""
+                      name="input"
+                      placeholder="Content here"
+                      type="textarea"
+                      {...field}
+                    />
+
+                    {meta.touched && meta.error && (
+                      <ErrorHolder>
+                        <Error>{meta.error}</Error>
+                      </ErrorHolder>
+                    )}
+                  </div>
+                )}
+              </Field>
+            </div>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
         <Button
-          model={"filled"}
-          theme="black"
-          text={edit ? "SAVE" : "CREATE"}
-          onClick={onSubmit}
+          model="filled"
+          theme={(!!values.title && !!values.content) ? "black" : "primary"}
+          text="ENTER"
+          type="submit"
+          disabled={!(!!values.title && !!values.content)}
         />
       </div>
+          </ModifyPostForm>
+        )}
+      </Formik>
+
+      
     </Container>
   );
 };
