869caededb06b8d29fbf4c62aabe47f0885f5346
Luiz Doretto <luiz.n.doretto@gmail.com>
2022-09-01 23:59:03 -0300

    remove console

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  useEffect(() => {console.log(isLoggedIn)}, [])
+
   return isLoggedIn ? <Outlet /> : <Navigate to="/" replace />;
 };
 
