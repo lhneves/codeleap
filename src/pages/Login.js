488b4aad57c4ac283a766a34fd5ffea17cf9735f
Luiz Doretto <luiz.n.doretto@gmail.com>
2022-09-02 00:53:42 -0300

    add first try of login user

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
 
 import ModalLogin from "../components/ModalLogin";
 
+import { useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import * as actions from '../actions/auth'
+
 function Login() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault()
-    navigate('home')
+    
+    dispatch(actions.login({user: e.target.value}))
+
+    navigate("home", { replace: true });
   }
 
   return (
