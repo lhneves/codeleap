8ba6df544547abc19feb2e86cc81e226c91401cc
Luiz Doretto <luiz.n.doretto@gmail.com>
2022-09-02 16:42:46 -0300

    refactor home page (posts, alerts, request,toasts)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ReactLoading from "react-loading";
+
 import Modal from "../../components/Modal";
 import ModalAlert from "../../components/ModalAlert/modalAlert";
 import ModifyPost from "../../components/ModifyPost";
-
 import Post from "../../components/Post";
+import { toast } from "react-toastify";
 
 import { Container } from "./home.styles";
 
+import axios from "../../services/axios";
+import moment from "moment";
+
+import { useFetch } from "../../hooks/useFetch";
+import { useSWRConfig } from "swr";
+import { colors } from "../../styles/variables";
+
 const Home = () => {
+  const { mutate } = useSWRConfig();
+
+  const { data, isLoading } = useFetch("/careers/");
+
+  const [openEditModal, setOpenEditModal] = useState(false);
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+
+  const [selectedPostId, setselectedPostId] = useState(null);
+
+  const handleEditPost = (e, id) => {
+    setselectedPostId(id);
+    setOpenEditModal(true);
+  };
+
+  const handleDeleteAlert = (e, id) => {
+    setselectedPostId(id);
+    setOpenDeleteModal(true);
+  };
+
+  const handleDeletePost = async () => {
+    const response = await axios
+      .delete(`/careers/${selectedPostId}/`, {})
+      .catch((err) => {
+        toast.error("Error on request :/");
+      });
+
+    if (response) {
+      toast.success("Post has been deleted succesfully.");
+      mutate("/careers/");
+      setOpenDeleteModal(false);
+    }
+  };
+
   return (
     <>
       <Container>
         <ModifyPost />
-        <Post
-          title="My First Post at CodeLeap Network!"
-          user="@lhneves"
-          date="25 minutes ago"
-          text="Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
+        {isLoading && (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <ReactLoading
+              type={"spin"}
+              color={colors.primary}
+              height={"10%"}
+              width={"10%"}
+            />
+          </div>
+        )}
 
-      Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat."
-        />
+        {!!data &&
+          data["results"].map(
+            ({ id, title, content, username, created_datetime }) => (
+              <Post
+                key={id}
+                title={title}
+                username={username}
+                date={`${moment(created_datetime).fromNow()}`}
+                text={content}
+                onEdit={(e) => handleEditPost(e, id)}
+                onDelete={(e) => handleDeleteAlert(e, id)}
+              />
+            )
+          )}
       </Container>
-      <Modal open={false}>
-        <ModifyPost edit />
+      <Modal open={openEditModal} onClose={() => setOpenEditModal(false)}>
+        <ModifyPost
+          edit
+          id={selectedPostId}
+          hasEdited={({ edited }) => setOpenEditModal(!edited)}
+        />
       </Modal>
-      <ModalAlert open={true} />
+      <ModalAlert
+        open={openDeleteModal}
+        onCancel={() => setOpenDeleteModal(false)}
+        onAccept={handleDeletePost}
+      />
     </>
   );
 };
